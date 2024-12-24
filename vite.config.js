e858c2aa8f5f671a43fd2636f86f76c609d7ddad
sejal singh <sssingh6305@gmail.com>
2024-12-24 11:51:34 +0530

    last commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
   proxy:{
-'/api':'https://ecommerce-letshop-backend.onrender.com',
+'/api':'https://letsshop-backend-6.onrender.com',
 
   },
     
